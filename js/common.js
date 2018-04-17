a9d9ecffd22def56df854ccc0930a69b0c6fc842
vkrotkikh <krotkikhvadim@gmail.com>
2018-04-17 16:46:43 +0300

    home page sections html start

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 (function ($) {
 
-'use strict';
+	'use strict';
 
-$(document).ready(function() {
+	$(document).ready(function() {
 
 	// SCROLL TO TOP
 	$('.scroll-top').on('click', function() {
@@ -26,33 +26,41 @@ $(document).ready(function() {
 		}
 	});
 
+	if($('.interface-slider').length) {
+		$('.interface-slider').slick({
+			slidesToShow: 1,
+			nextArrow: '<div class="interface-slider-arrow interface-slider-arrow-next"><i class="icon-arrow-right"></i></div>',
+			prevArrow: '<div class="interface-slider-arrow interface-slider-arrow-prev"><i class="icon-arrow-right"></i></div>',
+		});
+	}
+
 
 });
 
 
 
-function customSelect(){
+	function customSelect(){
 
-	$('.js-custom-select').on('click' , function(){
+		$('.js-custom-select').on('click' , function(){
 			customDropdownClose();
 
 			$(this).parent().find('.custom-dropdown').fadeIn();
 			$(this).parent().find('.custom-dropdown').addClass('-open');
-	});
+		});
 
-	$('.js-custom-dropdown_close').on('click' , function(){
-		customDropdownClose();
-	});
+		$('.js-custom-dropdown_close').on('click' , function(){
+			customDropdownClose();
+		});
 
-	$('.js-filter-search').on('keyup' , function(){
+		$('.js-filter-search').on('keyup' , function(){
 
-		var thisValue = $(this).val().toLowerCase(),
-			  itemsList = $(this).parents('.custom-dropdown').find('.js-filter-list .currency-item');
+			var thisValue = $(this).val().toLowerCase(),
+			itemsList = $(this).parents('.custom-dropdown').find('.js-filter-list .currency-item');
 
-		itemsList.each(function(index, el) {
+			itemsList.each(function(index, el) {
 
 				var thisTitle = $(this).find('.currency-title').text().toLowerCase(),
-					  thisSubTitle = $(this).find('.currency-subtitle').text().toLowerCase();
+				thisSubTitle = $(this).find('.currency-subtitle').text().toLowerCase();
 
 
 				if(!thisTitle.includes(thisValue) && !thisSubTitle.includes(thisValue)) {
@@ -61,62 +69,62 @@ function customSelect(){
 					$(this).parent().removeClass('d-none');
 					
 				}
+			});
+
 		});
 
-	});
+		$('.js-currency-link').on('click' , function(){
 
-	$('.js-currency-link').on('click' , function(){
+			var thisTitle = $(this).find('.currency-title').text(),
+			thisIconClass = $(this).find('.currency-icon i').attr("class"),
+			customSelectTitle = $(this).parents('.form-group_wrapper').find('.js-custom-select .currency-title'),
+			customSelectIconClass = $(this).parents('.form-group_wrapper').find('.js-custom-select .currency-icon i');
 
-		var thisTitle = $(this).find('.currency-title').text(),
-				thisIconClass = $(this).find('.currency-icon i').attr("class"),
-				customSelectTitle = $(this).parents('.form-group_wrapper').find('.js-custom-select .currency-title'),
-				customSelectIconClass = $(this).parents('.form-group_wrapper').find('.js-custom-select .currency-icon i');
+			$(this).parents('.custom-dropdown').fadeOut();
+			$(this).parents('.custom-dropdown').removeClass('-open');
 
-		$(this).parents('.custom-dropdown').fadeOut();
-		$(this).parents('.custom-dropdown').removeClass('-open');
+			customSelectTitle.text(thisTitle);
+			customSelectIconClass.attr('class', thisIconClass )
 
-		customSelectTitle.text(thisTitle);
-		customSelectIconClass.attr('class', thisIconClass )
+		});
 
-	});
-
- 	$(document).click(function(event) {
+		$(document).click(function(event) {
 	  //if you click on anything except the modal itself or the "open modal" link, close the modal
 	  if (!$(event.target).closest(".custom-dropdown, .js-custom-select").length) {
 	  	customDropdownClose();
 	  }
 	});
 
-}
+	}
 
-function customDropdownClose() {
+	function customDropdownClose() {
 		$('.custom-dropdown').fadeOut();
 		$('.custom-dropdown').removeClass('-open');
-}
+	}
 
-function exchangeSwap() {
+	function exchangeSwap() {
 
-	$('.js-exhange-swap').on('click' , function(){
+		$('.js-exhange-swap').on('click' , function(){
 
-		var sendValue = $('.js-send-value .js-custom-select-value').val(),
-				sendCurrencyTitle =  $('.js-send-value .js-custom-select .currency-title').text(),
-				sendIconClass = $('.js-send-value .js-custom-select .currency-icon i').attr('class'),
-				getValue = $('.js-get-value .js-custom-select-value').val(),
-				getCurrencyTitle =  $('.js-get-value .js-custom-select .currency-title').text(),
-				getIconClass = $('.js-get-value .js-custom-select .currency-icon i').attr('class');
-
-
-				
-		$('.js-send-value .js-custom-select-value').val( getValue);
-		$('.js-send-value .js-custom-select .currency-title').text(getCurrencyTitle);
-		$('.js-send-value .js-custom-select .currency-icon i').attr('class', getIconClass );
+			var sendValue = $('.js-send-value .js-custom-select-value').val(),
+			sendCurrencyTitle =  $('.js-send-value .js-custom-select .currency-title').text(),
+			sendIconClass = $('.js-send-value .js-custom-select .currency-icon i').attr('class'),
+			getValue = $('.js-get-value .js-custom-select-value').val(),
+			getCurrencyTitle =  $('.js-get-value .js-custom-select .currency-title').text(),
+			getIconClass = $('.js-get-value .js-custom-select .currency-icon i').attr('class');
 
 
-		$('.js-get-value .js-custom-select-value').val( sendValue);
-		$('.js-get-value .js-custom-select .currency-title').text(sendCurrencyTitle);
-		$('.js-get-value .js-custom-select .currency-icon i').attr('class', sendIconClass );
 
-	});
-}
+			$('.js-send-value .js-custom-select-value').val( getValue);
+			$('.js-send-value .js-custom-select .currency-title').text(getCurrencyTitle);
+			$('.js-send-value .js-custom-select .currency-icon i').attr('class', getIconClass );
+
+
+			$('.js-get-value .js-custom-select-value').val( sendValue);
+			$('.js-get-value .js-custom-select .currency-title').text(sendCurrencyTitle);
+			$('.js-get-value .js-custom-select .currency-icon i').attr('class', sendIconClass );
+
+		});
+	}
 
 }(jQuery));
